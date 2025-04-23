1adcba9b02219c4f4a0cf6cc3c2d3b74250cae77
maicol andres vaca gonzelez <cris123eq@gmail.com>
2025-04-23 14:47:29 -0500

    Se realiza la primera peticioń

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './Login.css'
 import { useNavigate } from 'react-router-dom'
 import { alertaGenerica, alertaRedireccion, generarToken } from '../helper/funciones'
 export default function Login(){
+   let urlUsers ="https://backjsonserverk.onrender.com/usuarios"
 
     const [getUsername, setUsername]=useState("")
     const [getPassword, setPassword]=useState("")
+    const [getUser, setUser]=useState([])
     let redireccion = useNavigate()
 
-    
+    function getUsuarios(){
+      fetch(urlUsers)
+        .then(response => response.json())
+        .then(data => setUser(data))
+        .catch(error => console.log(error))
+    }
+    useEffect(()=>{
+      getUsuarios()
+    },[])
 
-    function iniciarSesion(username, password){
-        if (username === 'admin' && password === '123456'){
+    function findUser(){
+      let user = getUser.find((item)=> getUser == item.user && getPassword == item.password)
+      return user;
+    }
+
+    function iniciarSesion(){
+        if (findUser){
           let tokenAcceso = generarToken()
           localStorage.setItem("token", tokenAcceso)
             alertaRedireccion(redireccion, "Bienvenido", "Sera redireccionado al home", "success", "/home")
@@ -30,7 +45,7 @@ export default function Login(){
             <div className="form_details">Login</div>
             <input onChange={(e) => setUsername(e.target.value)} placeholder="Username" className="input" type="text" />
             <input onChange={(e) => setPassword(e.target.value)} placeholder="Password" className="input" type="text" />
-            <button onClick={() => iniciarSesion(getUsername, getPassword)} type='button' className="btn">Login</button>
+            <button onClick={iniciarSesion} type='button' className="btn">Login</button>
             <span className="switch">Don't have an account? 
               <label className="signup_tog" htmlFor="signup_toggle">
                 Sign Up
